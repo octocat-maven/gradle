d4713855dcddf91b076b4fb2dd9ec3eb10cc9210
Stefan Wolf <wolf@gradle.com>
2019-09-24 21:49:48 +0200

    Take destroyables into account

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/execution/ExecuteActionsTaskExecuter.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/execution/ExecuteActionsTaskExecuter.java
@@ -358,6 +358,7 @@ public class ExecuteActionsTaskExecuter implements TaskExecuter {
             visitOutputProperties(((propertyName, type, roots) -> {
                 roots.forEach(root -> builder.add(root.getAbsolutePath()));
             }));
+            context.getTaskProperties().getDestroyableFiles().forEach(file -> builder.add(file.getAbsolutePath()));
             return Optional.of(builder.build());
         }
 
